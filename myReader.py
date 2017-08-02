257e531ced2adea6fc51be12654cbf20e8c8b941
YuanchenZhu <yz10g11@gmail.com>
2017-08-02 23:13:08 +0800

    prtotype01

--- myReader.py
+++ myReader.py
@@ -4,9 +4,10 @@ data = fr.readlines()
 
 print data
 print "Your name:" + data[0]
-
+line = raw_input("Please enter the number of line to be modified:")
+string = raw_input("Please enter the content in that line: \n") 
 # now change the 2nd line, note that you have to add a newline
-data[1] = 'mage\n'
+data[int(line)] = string + "\n"
 fr.close()
 
 fw = open('state.txt','w')
